ac089669e54d116f40137d39486cc40a5610bc35
tommgarciag <56352971+tommgarciag@users.noreply.github.com>
2020-05-04 01:43:59 -0300

    modifica la tarea 1 de la clase 5

--- tareas/clase-5/tarea1.js
+++ tareas/clase-5/tarea1.js
@@ -1,12 +1,43 @@
-document.querySelector('#calcular-salario-mensual').onclick = function(){
-    const salarioAnual = Number(document.querySelector('#salario-anual').value);
-    const salarioMensual = calcularSalarioMensual(salarioAnual);
+/*
+Cosas a tener en cuenta:
+1. Los <input> no tienen .innerText, en vez de eso, usan .value. https://developer.mozilla.org/es/docs/Web/HTML/Elemento/input
 
-    document.querySelector('#salario-mensual').value = salarioMensual;
+2. Los demás elementos usan .innerText para acceder y modificar al texto que aparece dentro. https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement
+También pueden usar .textContent, las diferencias no son importantes por ahora.
 
+3. Para evitar que el formulario <form> se “mande” y por ende recargue la página,
+al event handler “onclick”, agréguentle un return false; al final de la función.
+
+Ejemplo:
+const $botonCalcular = document.querySelector("#boton-calcular");
+
+$botonCalcular.onclick = function()
+    // código que se ejecuta cuando le hagan click a este elemento...
     return false;
 }
+*/
+
+//TAREA: completar tareas/clase-5/index.html para que incluya tarea-clase-5.js
+//TAREA: crear un formulario donde un usuario pueda ingresar su salario anual.
+//cuando el usuario haga click en el botón "calcular", mostrar el salario mensual
+// en una caja de texto deshabilitada. --> <input type="text" disabled id="salario-mensual"/>
 
 function calcularSalarioMensual(salarioAnual){
     return salarioAnual / 12;
-}
+  }
+  
+  // agrego $ para diferenciar un elemento html con la funcion calcularSalarioMensual
+  const $calcularSalarioMensual = document.querySelector("#calcular-salario-mensual");
+  
+  $calcularSalarioMensual.onclick = function(){
+    //console.log("Me hiceron click");
+    const salarioAnual = Number(document.querySelector("#salario-anual").value);
+    //console.log(salarioAnual);
+    const salarioMensual = calcularSalarioMensual(salarioAnual);
+    document.querySelector("#salario-mensual").value = salarioMensual;
+  
+  
+    return false; // para que no cargue el formulario
+                  // tambien puedo ponerle al button type="button" en el html 
+  }
+  
